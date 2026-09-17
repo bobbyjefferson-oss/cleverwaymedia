c7703e3e8d025d7662a561f7283441cdab916d1e
bobbyjefferson-oss <bouabidmarwan97@gmail.com>
2026-09-17 09:13:43 +0200

    Delete cleverway directory

--- cleverway/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
-
-export default createMiddleware(routing);
-
-export const config = {
-  // Match all paths except API, static files, and Next internals
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
-};
